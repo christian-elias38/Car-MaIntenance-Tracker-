16c608622c77c8de6b208b2ce4286d898b53ca4c
chr1133 <christianelias102@gmail.com>
2026-09-18 11:03:46 +0300

    feat: implement application theme and initial feature screens

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -32,7 +32,7 @@ class AppTheme {
       cardTheme: CardThemeData(
         color: AppColors.lightCard,
         elevation: 1.5,
-        shadowColor: Colors.black.withOpacity(0.06),
+        shadowColor: Colors.black.withValues(alpha: 0.06),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
           side: const BorderSide(color: AppColors.lightBorder, width: 1),
