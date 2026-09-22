23e45cdf0fa2734df1ec25c31b0966b2edd79716
chr1133 <christianelias102@gmail.com>
2026-09-22 19:18:18 +0300

    style: add AppVehicleImage widget for handling vehicle images with fallback support

---
+++ lib/features/vehicles/presentation/widgets/app_vehicle_image.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import '../../../../core/theme/app_colors.dart';
+
+class AppVehicleImage extends StatelessWidget {
+  final String imagePath;
+  final double? width;
+  final double? height;
+  final BoxFit fit;
+  final BorderRadius? borderRadius;
+
+  const AppVehicleImage({
+    super.key,
+    required this.imagePath,
+    this.width,
+    this.height,
+    this.fit = BoxFit.cover,
+    this.borderRadius,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Widget imageWidget;
+
+    final trimmedPath = imagePath.trim();
+
+    if (trimmedPath.startsWith('http://') || trimmedPath.startsWith('https://')) {
+      imageWidget = Image.network(
+        trimmedPath,
+        width: width,
+        height: height,
+        fit: fit,
+        errorBuilder: (ctx, err, stack) => _buildFallback(context),
+      );
+    } else if (trimmedPath.startsWith('assets/')) {
+      imageWidget = Image.asset(
+        trimmedPath,
+        width: width,
+        height: height,
+        fit: fit,
+        errorBuilder: (ctx, err, stack) => _buildFallback(context),
+      );
+    } else if (trimmedPath.isNotEmpty) {
+      if (!kIsWeb) {
+        final file = File(trimmedPath);
+        if (file.existsSync()) {
+          imageWidget = Image.file(
+            file,
+            width: width,
+            height: height,
+            fit: fit,
+            errorBuilder: (ctx, err, stack) => _buildFallback(context),
+          );
+        } else {
+          imageWidget = _buildFallback(context);
+        }
+      } else {
+        imageWidget = _buildFallback(context);
+      }
+    } else {
+      imageWidget = _buildFallback(context);
+    }
+
+    if (borderRadius != null) {
+      return ClipRRect(
+        borderRadius: borderRadius!,
+        child: imageWidget,
+      );
+    }
+
+    return imageWidget;
+  }
+
+  Widget _buildFallback(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    return Container(
+      width: width,
+      height: height,
+      color: isDark ? AppColors.darkSurface : AppColors.mintBackground,
+      child: Center(
+        child: Icon(
+          Icons.directions_car_filled_rounded,
+          size: (height != null && height! < 60) ? 24 : 38,
+          color: AppColors.primaryLight,
+        ),
+      ),
+    );
+  }
+}
